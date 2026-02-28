b8521ac069637a444990136252b7def5fa97bf8a
varunchitrala <varunchitralaa@gmail.com>
2026-02-28 15:48:28 +0530

    Cleanup debug test files

--- backend/test_email.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const emailService = require('./src/services/emailService');
-
-async function run() {
-    console.log("Testing email...");
-    const res = await emailService.sendEmail('test@example.com', 'Test', '<h1>Test</h1>');
-    console.log("Result:", res);
-}
-run();
